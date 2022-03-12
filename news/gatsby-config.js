f170ed3a318d759b064a6b56a90056942ef49957
santiak3 <71164211+santiak3@users.noreply.github.com>
2022-03-12 16:44:13 -0600

    Added markdown support

--- news/gatsby-config.js
+++ news/gatsby-config.js
@@ -31,8 +31,8 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
