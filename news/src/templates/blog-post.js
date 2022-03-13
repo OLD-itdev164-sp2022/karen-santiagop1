502b30444d34606c0bd75decb79857fc012d4df1
santiak3 <71164211+santiak3@users.noreply.github.com>
2022-03-12 18:53:29 -0600

    Updated Graphql & blogpost to display body

--- news/src/templates/blog-post.js
+++ news/src/templates/blog-post.js
@@ -3,13 +3,12 @@ import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 
 const BlogPost = ({ data }) => {
-    const { title } = data.contentfulBlogPost;
+    const { title, body } = data.contentfulBlogPost;
 
     return (
         <Layout>
-            <h1>
-                {title}
-            </h1>
+            <h1>{title}</h1>
+            <div dangerouslySetInnerHTML={{__html:body.childMarkdownRemark.html}}></div>
         </Layout>
     );
 }
@@ -18,9 +17,14 @@ export default BlogPost;
 
 export const pageQuery = graphql`
 query blogPostQuery($slug: String!) {
-    contentfulBlogPost(slug: {eq: $slug}) {
-        title 
+	contentfulBlogPost(slug: {eq: $slug}){
+		title
         slug
+        body {
+                childMarkdownRemark {
+                    html
+        }
     }
+}
 }
 `
